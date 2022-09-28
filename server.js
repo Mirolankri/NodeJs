9ecdc3b215e139b141356eb5248d2aa1ea555a3f
Mirolankri <59894607+Mirolankri@users.noreply.github.com>
2022-09-28 22:28:37 +0300

    FirstProject

---
+++ server.js
@@ -0,0 +1,4 @@
+const FirstName = require("./exports/FirstName.export")
+const obj = require("./exports/LastName.export")
+
+console.log(`My First Name is ${FirstName} And My Last Name is ${obj.LastName}`);
