4524710810889c8c2e09ce67a595d0ae56d4a5c7
Miros-iStore <>
2022-10-03 01:21:24 +0300

    node.js core modules

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
-const FirstName = require("./exports/FirstName.export")
-const obj = require("./exports/LastName.export")
+const FileSystem = require("fs");
+const path = require('path')
+const { mkdir, readdir, writeFile, rmdir, unlink } = require("fs/promises");
+const user = [{name: 'Ben', last: 'Krakov'},{name: 'Miro', last: 'Lankri'},{name: 'Yinon', last: 'Peretz'}];
+const ThisDir = `${__dirname}/users`
+const removeFilesAndFolder = async ()=>
+{
+    readdir(`${ThisDir}`).then(filenames => {
+        for (let filename of filenames) if(path.extname(filename) == ".txt")unlink(`${ThisDir}/${filename}`)
 
-console.log(`My First Name is ${FirstName} And My Last Name is ${obj.LastName}`);
+        readdir(`${ThisDir}`).then(Files=>{
+            if(!Files.length) rmdir(`${ThisDir}`)
+            else console.log(`Folder Not Empty Please See The File \n >> ${Files}`);
+        })
+    }).catch(err=>{
+        console.log(err.message);
+    })
+}
+
+const makeAndRemoveFileAndFolder = async ()=>
+{
+try {
+   await readdir(`${ThisDir}`)
+        removeFilesAndFolder()
+    } catch (error) {
+        await mkdir(`${ThisDir}`)
+            user.forEach(user=>{
+                writeFile(`${ThisDir}/${user.name}-${user.last}.txt`,`Name: ${user.name} Last:${user.last}`)
+            })
+        setTimeout(() => {
+            removeFilesAndFolder()
+        }, 5000);
+    }
+}
+
+makeAndRemoveFileAndFolder()
