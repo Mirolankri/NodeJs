d2cf819266d3fd4b4e58f57c629175aad613cc86
Mirolankri <59894607+Mirolankri@users.noreply.github.com>
2022-10-18 11:13:31 +0300

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-const FirstName = require("./exports/FirstName.export")
-const obj = require("./exports/LastName.export")
-
-console.log(`My First Name is ${FirstName} And My Last Name is ${obj.LastName}`);
